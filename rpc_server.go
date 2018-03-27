3d7710f4400123e2ef536351811bd73193bf84e4
yangdawei <dawei.yang@sinobbd>
2018-03-27 14:54:38 +0800

    rpc example

---
+++ rpc_server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/rpc"
+)
+
+type Server struct{}
+
+func (s *Server) Negate(i int64, reply *int64) error{
+	*reply = i + 1
+	return nil
+}
+
+func server(){
+	rpc.Register(new(Server))
+	ln, err := net.Listen("tcp", ":9999")
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		c, err := ln.Accept()
+		if err != nil{
+			continue
+		}
+		go rpc.ServeConn(c)
+	}
+}
+
+func main(){
+	server()
+}
